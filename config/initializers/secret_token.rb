99e139d5133a3b1559224f3ff8d3e5cc7ad008ca
gregorbarth <andi@portaputer.lan>
2011-07-29 12:54:46 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CenatusCms::Application.config.secret_token = '41a14d8e628404ec95bf41cbb1cc8c1473d186bcc06784299aeebb8d87aa13ff7d17e9e30180c212701cd37d02aa239a612e6c7a1ec1b065467fd0895e2dc041'
