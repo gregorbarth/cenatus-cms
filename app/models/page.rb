9e2594b1cbdf3fc99a5181d7aa6e779c616eefdd
msp <info@radialsolutions.co.uk>
2011-08-11 19:23:07 +0100

    RA config

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,6 +4,33 @@ class Page < ActiveRecord::Base
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :assets
   
-  validates_presence_of :name, :slug
+  validates_presence_of :name, :slug 
   
+  
+                        
+  rails_admin do
+    show do
+      field :id
+      field :name
+      field :slug
+
+      # field :website_url do
+      #   formatted_value do
+      #     if value.blank?
+      #       "EMPTY"
+      #     else
+      #       bindings[:view].link_to value, value
+      #     end
+      #   end
+      # end
+
+      field :content_areas
+      field :assets, :has_and_belongs_to_many_association
+      field :categories, :has_and_belongs_to_many_association
+
+      field :created_at
+      field :updated_at
+
+    end  
+  end
 end
