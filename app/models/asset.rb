49b2eb2888856f532d9fdcbdea4a0384066d550b
msp <info@radialsolutions.co.uk>
2011-08-11 19:16:37 +0100

    Validation

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,6 +2,8 @@ class Asset < ActiveRecord::Base
   
   has_and_belongs_to_many :pages
   
+  validates :name, :presence => true
+  
   # has_attached_file :asset, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :bucket => 'virgin.s3.amazonaws.com', :path => ":attachment/:id/:style.:extension", :styles => { :medium => "120x120>", :thumb => "40x40>" }
   has_attached_file :asset, :styles => { :medium => "120x120>", :thumb => "40x40>" }
   
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Asset do
+  
+  before :each do
+    # Asset.stub!(:find).and_return(mock_model(Asset))
+    # @asset = mock_model(Asset)
+  end
+  
   it "can be instantiated" do
     Asset.new.should be_an_instance_of(Asset)
   end                                           
@@ -11,9 +17,14 @@ describe Asset do
   
   it "has many pages" do
     Asset.reflect_on_association(:pages).should_not be_nil
-    @asset = Asset.new
-    @asset.pages.length == 0
+    @asset_assoc = Asset.new
+    @asset_assoc.pages.length == 0
   end
   
+  it "has a name that is required" do
+    defined?(:name).should_not be_nil
+    @asset = Asset.new
+    @asset.should_not be_valid
+  end  
   
 end
