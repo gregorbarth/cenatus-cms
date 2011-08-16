72860c1815e11c176fd898bec35fc1d85c3d5d0e
msp <info@radialsolutions.co.uk>
2011-08-16 15:06:17 +0100

    Don't compile Sass on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ CenatusCms::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # use the VCS files for prod
+  Sass::Plugin.options[:never_update] = true   
+  
 end
